055613e43f43df3d4add840c714f91e92ed7b614
MaxiJRom <maxiromero11@hotmail.com>
2023-01-29 14:21:24 +0100

    cambios de la barra
    la barra de navegacion ahora es toggleada

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const burgerButton = document.querySelector(".mobile-nav-toggle");
+const primaryNav= document.querySelector(".primary-navigation");
+
+burgerButton.addEventListener("click", ()=>{
+    const visibility=primaryNav.getAttribute("data-visible");
+
+    if (visibility==="false"){
+  primaryNav.setAttribute("data-visible","true");
+        burgerButton.setAttribute("aria-expanded", true);
+}
+else if (visibility === "true"){
+    primaryNav.setAttribute("data-visible","false");
+    burgerButton.setAttribute("aria-expanded", false);
+}
+
+})
