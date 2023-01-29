6296f49d64535cab591f5e41fed687d8a9f3f7a4
MaxiJRom <maxiromero11@hotmail.com>
2023-01-29 15:14:11 +0100

    s
    s

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 const burgerButton = document.querySelector(".mobile-nav-toggle");
-const primaryNav= document.querySelector(".primary-navigation");
+const primaryNav = document.querySelector(".primary-navigation");
 
-burgerButton.addEventListener("click", ()=>{
-    const visibility=primaryNav.getAttribute("data-visible");
+burgerButton.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
 
-    if (visibility==="false"){
-  primaryNav.setAttribute("data-visible","true");
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", "true");
         burgerButton.setAttribute("aria-expanded", true);
-}
-else if (visibility === "true"){
-    primaryNav.setAttribute("data-visible","false");
-    burgerButton.setAttribute("aria-expanded", false);
-}
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        burgerButton.setAttribute("aria-expanded", false);
+    }
 
 })
